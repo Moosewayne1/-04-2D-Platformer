61cf6d5f0e7efb1749cd02037b5f261bdefb2ce6
Moosewayne1 <70243391+Moosewayne1@users.noreply.github.com>
2020-10-30 19:14:09 -0500

    Add files via upload

---
+++ Global.gd
@@ -0,0 +1,36 @@
+extends Node
+
+var score = 0
+var lives = 5
+var max_lives = 5
+var level = 1
+var health = 100
+var max_health = 100
+
+func _unhandled_input(event):
+	if event.is_action_pressed("menu"):
+		var menu = get_node_or_null("root/Game/UI/Menu")
+		if menu != null:
+			var p = get_tree().paused
+			if p:
+				menu.hide()
+				get_tree().paused = false 
+			else:
+				menu.show()
+				get_tree().paused = true
+		
+
+func _ready():
+	pass
+
+func increase_score(s):
+	score += s
+
+func decrease_health(h):
+	health -= h
+
+func decrease_lives(l):
+	lives -= l
+	health = max_health
+	if lives <= 0:
+		get_tree().change_scene("res://Levels/Game_Over.tscn")
